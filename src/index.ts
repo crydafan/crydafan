be891cf47a59f58d1089a44931d70b2213729bc7
Leonardo <leonardo.estacio.h@gmail.com>
2025-11-19 18:33:47 -0500

    feat: create server with express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log("Hello, World!");
+import express from "express";
+import path from "path";
+import { configDotenv } from "dotenv";
+
+configDotenv({
+  quiet: false,
+  debug: true,
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "..", "public")));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
