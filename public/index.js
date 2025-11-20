f16e4b8c606adefa7c401941978849a2e49572e6
Leonardo <leonardo.estacio.h@gmail.com>
2025-11-19 19:32:01 -0500

    feat: show poem using script

---
+++ public/index.js
@@ -0,0 +1,9 @@
+const poem = `
+Roses are red,<br/>
+Violets are blue,<br/>
+Coding is fun,<br/>
+And so are you.</br/>
+`;
+
+const poemElement = document.getElementById("poem");
+poemElement.innerHTML = poem;
