c50efacfabd9a665066c6ff09f598318a011b26e
Leonardo <leonardo.estacio.h@gmail.com>
2026-01-01 05:16:40 -0500

    chore: mark req parameter as unused

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,7 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-app.get("/api/generate", async (req: Request, res: Response) => {
+app.get("/api/generate", async (_req: Request, res: Response) => {
   const { textStream } = streamText({
     model: openai("gpt-4o"),
     prompt: `Please generate a poem that contains short lyric fragments of Daft Punk's songs. Put the fragments between <b></b> and also add line breaks as <br/> and don't put quotation marks between the fragments. Return the response in plain text.`,
