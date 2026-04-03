709560a61464f48e715d91be75ca3a5d73c3b1bb
Leonardo <leonardo.estacio.h@gmail.com>
2026-04-02 21:20:45 -0500

    feat: remove ai functionality

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-import express, { Request, Response } from "express";
+import express from "express";
 import path from "path";
 import { configDotenv } from "dotenv";
-import { streamText } from "ai";
-import { openai } from "@ai-sdk/openai";
 
 configDotenv({
   quiet: false,
@@ -16,17 +14,4 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-});
-
-app.get("/api/generate", async (_req: Request, res: Response) => {
-  const { textStream } = streamText({
-    model: openai("gpt-4o"),
-    prompt: `Please generate a poem that contains short lyric fragments of Daft Punk's songs. Put the fragments between <b></b> and also add line breaks as <br/> and don't put quotation marks between the fragments. Return the response in plain text.`,
-  });
-
-  for await (const delta of textStream) {
-    res.write(delta);
-  }
-
-  res.end();
 });
